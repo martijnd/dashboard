c0d17dbda5b96fa2c78087a99b30aa4955b8da0a
martijnd <martijn.dorsman@gmail.com>
2019-07-20 14:10:31 +0200

    Basic grid setup

---
+++ src/app/api.service.ts
@@ -0,0 +1,4 @@
+export class ApiService {
+  public uurbestuurKey = '8jvAwFUfzXcQMoSJNGy5pwd7CNIOTVPFee1jmVkfNvxtyFWshRZKv5HvAC04';
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ApiService} from './api.service';
+import {BoardComponent} from './board/board.component';
+import {TileComponent} from './tile/tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    TileComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
